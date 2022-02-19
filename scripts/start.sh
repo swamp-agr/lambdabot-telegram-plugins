ad4f1e5e4170dc0f84cf55597d26d78d9b6f4e81
Andrey Prokopenko <persiantiger@yandex.ru>
2022-02-19 13:44:53 +0100

    Fix start script

--- scripts/start.sh
+++ scripts/start.sh
@@ -8,7 +8,7 @@ exec 2>&1
 # prepare log directory
 mkdir -p ./log
 
-telegram-lambdabot -l DEBUG +RTS -A64m -AL256m -qn2 -RTS >> log/telegram-lambdabot.log &
+telegram-lambdabot -l DEBUG +RTS -A64m -AL256m -qn2 -RTS >> log/telegram-lambdabot.log 2>&1 &
 
 # write PID to file
 echo $! > ./telegram-lambdabot.pid
